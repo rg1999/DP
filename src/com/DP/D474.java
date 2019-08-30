7727fc4aa2b9480698b40d3b5bab23278d2bfa99
madfr0st <suman.saurav117@gmail.com>
2019-08-30 12:54:49 +0530

    desktop!

--- src/com/DP/D474.java
+++ src/com/DP/D474.java
@@ -7,7 +7,7 @@ public class D474 {
     static int MODULO = 1000000007;
     public static void main(String[] args) throws IOException{
         BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter pot = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] s1 = inp.readLine().split(" ");
         int testCase = Integer.parseInt(s1[0]);
         int k = Integer.parseInt(s1[1]);
@@ -16,16 +16,35 @@ public class D474 {
             String[] s3 = inp.readLine().split(" ");
             int low = Integer.parseInt(s3[0]);
             int high = Integer.parseInt(s3[1]);
+            if(k<=high) {
+                int ans = 0;
+                int total = 0;
+                int[] dp = new int[high];
+                for (int i = 0; i < k; i++) {
+                    dp[i] = 1;
+                    if (i == k - 1) {
+                        dp[i]++;
+                    }
+                    if (i >= low - 1) {
+                        ans = ans + dp[i];
+                    }
+                }
+                for (int i = k; i < high; i++) {
 
-            int ans = 0;
-            int total = 0;
-            int[] dp = new int[high];
-            for(int i=0;i<high;i++){
-
-                
-
+                    dp[i] = (dp[i - 1] + dp[i - k])%MODULO;
+                    if (i >= low - 1) {
+                        ans = (ans + dp[i])%MODULO;
+                    }
+                }
+                if(ans<0){
+                    ans = ans +MODULO;
+                }
+               out.write(ans+"\n");
             }
-
+            else{
+                out.write((high-low+1)+"\n");
+            }
         }
+        out.flush();
     }
 }
